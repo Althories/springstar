676eff2836d109a337c2500da97ab8578938e3e7
Reedspun <reedspun@gmail.com>
2025-02-16 17:12:48 -0600

    Added Idle Bounce Anim. and Wait Time
    Idle bounce now has a period of waiting before actually bouncing. Animations play during this time. Also changed naming scheme for camPivot variable.

--- Scripts/SpringCharacter/spring_script.gd
+++ Scripts/SpringCharacter/spring_script.gd
@@ -5,25 +5,29 @@ const JUMP_IMPULSE = 4.5
 const CAMERA_VERTICAL_OFFSET = 1.0
 const CAMERA_INTERPOLATION_WEIGHT = 0.1
 const CAMERA_VERTICAL_MOVEMENT_DEADZONE = 1.2
+const IDLE_ANIM_SPEED = 0.6
+const IDLE_BOUNCE_WAIT_FRAMES = 20
 
-@onready var cam_pivot: Node3D = $CamPivot
+@onready var camPivot: Node3D = $CamPivot
 @onready var animationPlayer: AnimationPlayer = $SpringAnimation
 @export var sens = 0.15
 
 var can_ground_bounce = true
+var bounce_timer = 0
 var charge_velocity = 0
 var camera_anchor = Vector3(0, 2, 0)
-var slerp_y = 2.0
+var slerp_y = 0
 var most_recent_groundpoint = Vector3(0, 0, 0)
 
 func _ready():
+	animationPlayer.play_backwards("SpringSquish") # prevents error spam from having no animation while in the air
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sens))
-		cam_pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
-		cam_pivot.rotation.x = clamp(cam_pivot.rotation.x, deg_to_rad(-60), deg_to_rad(45))
+		camPivot.rotate_x(deg_to_rad(-event.relative.y * sens))
+		camPivot.rotation.x = clamp(camPivot.rotation.x, deg_to_rad(-60), deg_to_rad(45))
 	if event.is_action_pressed("ui_cancel"):	#bound to esc by default
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE	#mouse made visible, can go outside game window
 	if event.is_action_pressed("reset"):
@@ -38,17 +42,19 @@ func _physics_process(delta: float) -> void:
 	'''For anything to do with physics in the world'''
 	
 	if is_on_floor():
+		velocity = Vector3() # Kills ground velocity. May have to change for ragdoll or physics interactions.
 		most_recent_groundpoint = self.position
 		if Input.is_action_just_released("jump"):		#charge jump release
 			velocity.y = JUMP_IMPULSE/3 + charge_velocity 				#temporary until charge is handled
 			charge_velocity = 0							#reset charge velocity upon jump
-			animationPlayer.play_backwards("SpringSquish")				#Un charge spring movement		
 		elif not Input.is_action_pressed("jump"):
 			#Will fix jumping once this condition is revised
-			animationPlayer.play("SpringSquish")		#Squish spring
-			can_ground_bounce = true					#not starting a jump, ground bounce
-			velocity.y = JUMP_IMPULSE					#regular bounce impulse
-			animationPlayer.play_backwards("SpringSquish")			#Unsquish spring
+			bounce_timer += 1
+			if animationPlayer.current_animation_position == 0.0:
+				animationPlayer.play("SpringSquish",-1, IDLE_ANIM_SPEED) # begin idle bounce animation
+			if bounce_timer >= IDLE_BOUNCE_WAIT_FRAMES:
+				can_ground_bounce = true					#not starting a jump, ground bounce
+				velocity.y = JUMP_IMPULSE					#regular bounce impulse
 		elif Input.is_action_pressed("jump"):
 			can_ground_bounce = false					#stops ground movement upon charge start
 			velocity = Vector3.ZERO						#stop all movement, freeze in spot
@@ -58,6 +64,9 @@ func _physics_process(delta: float) -> void:
 				charge()								#call charge function
 		
 	if not is_on_floor():
+		bounce_timer = 0
+		if animationPlayer.current_animation_position != 0.0:
+			animationPlayer.play_backwards("SpringSquish")				#Un charge spring movement		
 		velocity += get_gravity() * delta				#gravity. Can be changed in settings			
 		can_ground_bounce = false						#keeps spring from using ground controls in the air
 	
