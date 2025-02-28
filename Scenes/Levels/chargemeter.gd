e0a5405e76ccb3ae5eddf6db806241d05bcf04e1
Lex Jahnsen <lex.jahnsen@gmail.com>
2025-02-28 11:15:23 -0600

    Charge not visible in air

--- Scenes/Levels/chargemeter.gd
+++ Scenes/Levels/chargemeter.gd
@@ -11,7 +11,8 @@ func update():
 	value = (spring.charge_velocity / 18.0) * max_value  # Scale charge % for UI
 	
 func _process(float):
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and spring.is_on_floor():
+		await get_tree().create_timer(0.2).timeout
 		visible = true
 	else: 
 		visible = false
