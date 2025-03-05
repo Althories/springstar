f66adc7d62d6afc54a006ec55c42dfd05ce8a7d8
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-03-04 21:09:56 -0600

    Revised AnimationTree
    No have two trees in a tree added together: one controls coils and the other controls rings 🎊

--- Scenes/SpringCharacter/spring_import.gd
+++ Scenes/SpringCharacter/spring_import.gd
@@ -1,17 +1,64 @@
 extends Node3D
 
 @onready var aniTree: AnimationTree = $AnimationTree
+@onready var masterTree: AnimationTree = $Hmmm
 
 var coilSpeed: float = 1.8
 var rotationSpeed: float = 1
 
 func _ready() -> void:
-	aniTree.active = true
+	aniTree.active = false#true
+	masterTree.active = true
 	aniTree["parameters/Charge/CoilSpeed/scale"] = coilSpeed #Charging coils
 	aniTree["parameters/Jump/Coil Speed/scale"] = coilSpeed-0.6 #Bouncing coils
 	
 
 func _process(_delta: float) -> void:
+	if(Input.is_action_just_pressed("move_forward")):
+		masterTree["parameters/CoilSpeed/scale"] = coilSpeed
+		masterTree["parameters/Coils/conditions/coil"] = true
+		masterTree["parameters/Coils/conditions/uncoil"] = true
+		
+		masterTree["parameters/Rings Rotation/conditions/charging"] = false
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
+		
+	elif(Input.is_action_pressed("move_back")):
+		if(not masterTree["parameters/Rings Rotation/conditions/charging"]):
+			rotationSpeed = 0.05
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
+		
+		masterTree["parameters/CoilSpeed/scale"] = coilSpeed - 0.6
+		masterTree["parameters/Coils/conditions/coil"] = true
+		masterTree["parameters/Coils/conditions/uncoil"] = false
+		
+		masterTree["parameters/Rings Rotation/conditions/charging"] = true
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = false
+		
+	elif(Input.is_action_just_released("move_back")):
+		masterTree["parameters/Coils/conditions/coil"] = false
+		masterTree["parameters/Coils/conditions/uncoil"] = true
+		
+		masterTree["parameters/Rings Rotation/conditions/charging"] = false
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
+
+	else:
+		masterTree["parameters/Coils/conditions/coil"] = false
+		masterTree["parameters/Coils/conditions/uncoil"] = true
+		
+		masterTree["parameters/Rings Rotation/conditions/charging"] = false
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
+		
+	if(masterTree["parameters/Rings Rotation/conditions/charging"]):
+		if(rotationSpeed < 2):
+			rotationSpeed += 0.01
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
+	else:
+		if(rotationSpeed > 1):
+			rotationSpeed -= 0.05
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
+	
+	
+	"""
 	if(Input.is_action_just_pressed("move_forward")):
 		aniTree["parameters/conditions/jump"] = true #Jump
 		aniTree["parameters/conditions/charging"] = false #Stop charge
@@ -41,3 +88,4 @@ func _process(_delta: float) -> void:
 		if(rotationSpeed > 1):
 			rotationSpeed -= 0.05
 			aniTree["parameters/Charge/RotateSpeed/scale"] = rotationSpeed
+	"""
