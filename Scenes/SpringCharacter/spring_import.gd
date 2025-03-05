1307ccb9b0430c8cd2c9e9425b126e63837df4f8
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-03-05 12:42:10 -0600

    Cleaned up animation, fine-tuned, added test rotating

--- Scenes/SpringCharacter/spring_import.gd
+++ Scenes/SpringCharacter/spring_import.gd
@@ -1,20 +1,35 @@
 extends Node3D
 
-@onready var aniTree: AnimationTree = $AnimationTree
-@onready var masterTree: AnimationTree = $Hmmm
+@onready var masterTree: AnimationTree = $AnimationTree
+@onready var spring: Node3D = $Spring
+@onready var innerRing: MeshInstance3D = $Capsule/InnerRing
+@onready var middleRing: MeshInstance3D = $Capsule/InnerRing/MiddleRing
+@onready var outerRing: MeshInstance3D = $Capsule/InnerRing/MiddleRing/OuterRing
 
-var coilSpeed: float = 1.8
+var coilSpeed: float = 2
 var rotationSpeed: float = 1
 
 func _ready() -> void:
-	aniTree.active = false#true
 	masterTree.active = true
-	aniTree["parameters/Charge/CoilSpeed/scale"] = coilSpeed #Charging coils
-	aniTree["parameters/Jump/Coil Speed/scale"] = coilSpeed-0.6 #Bouncing coils
 	
+func _process(delta: float) -> void:
+	#Basic spring rotation
+	if(Input.is_action_pressed("move_left")):
+		spring.rotate_x(-2*delta)
+	if(Input.is_action_pressed("move_right")):
+		spring.rotate_x(2*delta)
+	if(Input.is_action_pressed("jump")):
+		spring.rotate_z(-2*delta)
+		
+	#innerRing.rotate_x(1*delta)
+	#Rotate rings with spring
+	#outer/inner = x
+	#innerRing.rota = Vector3(spring.rotation.x,innerRing.rotation.y, innerRing.rotation.z)
+	
+	animate()
 
-func _process(_delta: float) -> void:
-	if(Input.is_action_just_pressed("move_forward")):
+func animate() -> void:
+	if(Input.is_action_just_pressed("move_forward")): #Bounce
 		masterTree["parameters/CoilSpeed/scale"] = coilSpeed
 		masterTree["parameters/Coils/conditions/coil"] = true
 		masterTree["parameters/Coils/conditions/uncoil"] = true
@@ -22,70 +37,44 @@ func _process(_delta: float) -> void:
 		masterTree["parameters/Rings Rotation/conditions/charging"] = false
 		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
 		
-	elif(Input.is_action_pressed("move_back")):
-		if(not masterTree["parameters/Rings Rotation/conditions/charging"]):
+	elif(Input.is_action_pressed("move_back")): #Charge start
+		#Lower intial ring rotation
+		if(masterTree["parameters/Rings Rotation/playback"].get_current_node() == "RESET" or
+		   masterTree["parameters/Rings Rotation/playback"].get_current_node() == "Start"):
 			rotationSpeed = 0.05
 			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
 		
-		masterTree["parameters/CoilSpeed/scale"] = coilSpeed - 0.6
+		masterTree["parameters/CoilSpeed/scale"] = coilSpeed - 1
 		masterTree["parameters/Coils/conditions/coil"] = true
 		masterTree["parameters/Coils/conditions/uncoil"] = false
 		
 		masterTree["parameters/Rings Rotation/conditions/charging"] = true
 		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = false
 		
-	elif(Input.is_action_just_released("move_back")):
+	elif(Input.is_action_just_released("move_back")): #Charge release
+		if(rotationSpeed < 0.5):
+			rotationSpeed = 0.5
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
 		masterTree["parameters/Coils/conditions/coil"] = false
 		masterTree["parameters/Coils/conditions/uncoil"] = true
 		
+		masterTree["parameters/CoilSpeed/scale"] = coilSpeed
 		masterTree["parameters/Rings Rotation/conditions/charging"] = false
 		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
 
-	else:
+	else:											#Nothing
 		masterTree["parameters/Coils/conditions/coil"] = false
 		masterTree["parameters/Coils/conditions/uncoil"] = true
 		
 		masterTree["parameters/Rings Rotation/conditions/charging"] = false
 		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
-		
+	
+	#Slow/speed up rotation speed
 	if(masterTree["parameters/Rings Rotation/conditions/charging"]):
-		if(rotationSpeed < 2):
+		if(rotationSpeed < 2.5):
 			rotationSpeed += 0.01
 			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
 	else:
 		if(rotationSpeed > 1):
 			rotationSpeed -= 0.05
 			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
-	
-	
-	"""
-	if(Input.is_action_just_pressed("move_forward")):
-		aniTree["parameters/conditions/jump"] = true #Jump
-		aniTree["parameters/conditions/charging"] = false #Stop charge
-	elif(Input.is_action_pressed("move_back")):
-		if(not aniTree["parameters/conditions/charging"]):
-			rotationSpeed = 0.05
-			aniTree["parameters/Charge/RotateSpeed/scale"] = rotationSpeed
-		aniTree["parameters/conditions/jump"] = false #Stop jump
-		#Set Blend stuff
-		aniTree["parameters/Charge/StateMachine/conditions/rotateFinish"] = false #Don't stop rotating
-		aniTree["parameters/Charge/StateMachine 2/conditions/Uncharge"] = false #Don't uncoil
-		aniTree["parameters/conditions/charging"] = true #Start charge
-	elif(Input.is_action_just_released("move_back")):
-		aniTree["parameters/conditions/jump"] = false #Stop jump
-		aniTree["parameters/conditions/charging"] = false #Stop charge
-		aniTree["parameters/Charge/StateMachine/conditions/rotateFinish"] = true #Finish rotation
-		aniTree["parameters/Charge/StateMachine 2/conditions/Uncharge"] = true #uncoil
-	else:
-		aniTree["parameters/conditions/jump"] = false #Stop jump
-		aniTree["parameters/conditions/charging"] = false #Stop charge
-		
-	if(aniTree["parameters/conditions/charging"]):
-		if(rotationSpeed < 2):
-			rotationSpeed += 0.01
-			aniTree["parameters/Charge/RotateSpeed/scale"] = rotationSpeed
-	else:
-		if(rotationSpeed > 1):
-			rotationSpeed -= 0.05
-			aniTree["parameters/Charge/RotateSpeed/scale"] = rotationSpeed
-	"""
