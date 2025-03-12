803ddc4fc3eb6eca9b0fa0723b475bc7f58caa17
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-03-12 13:31:54 -0500

    Revert "Cleaned up"
    This reverts commit 3ceb184c166005bbbd75874007201b6355c216a5.

---
+++ Scenes/SpringCharacter/spring_import.gd
@@ -0,0 +1,62 @@
+extends Node3D
+
+@onready var masterTree: AnimationTree = $AnimationTree
+
+var coilSpeed: float = 2
+var rotationSpeed: float = 1
+
+func _ready() -> void:
+	masterTree.active = true
+	
+func _process(delta: float) -> void:
+	animate()
+
+func animate() -> void:
+	if(Input.is_action_just_pressed("move_forward")): #Bounce
+		masterTree["parameters/CoilSpeed/scale"] = coilSpeed
+		masterTree["parameters/Coils/conditions/coil"] = true
+		masterTree["parameters/Coils/conditions/uncoil"] = true
+		
+		masterTree["parameters/Rings Rotation/conditions/charging"] = false
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
+		
+	elif(Input.is_action_pressed("move_back")): #Charge start
+		#Lower intial ring rotation
+		if(masterTree["parameters/Rings Rotation/playback"].get_current_node() == "RESET"):
+			rotationSpeed = 0.05
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
+		
+		masterTree["parameters/CoilSpeed/scale"] = coilSpeed - 1
+		masterTree["parameters/Coils/conditions/coil"] = true
+		masterTree["parameters/Coils/conditions/uncoil"] = false
+		
+		masterTree["parameters/Rings Rotation/conditions/charging"] = true
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = false
+		
+	elif(Input.is_action_just_released("move_back")): #Charge release
+		if(rotationSpeed < 0.5):
+			rotationSpeed = 0.5
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
+		masterTree["parameters/Coils/conditions/coil"] = false
+		masterTree["parameters/Coils/conditions/uncoil"] = true
+		
+		masterTree["parameters/CoilSpeed/scale"] = coilSpeed
+		masterTree["parameters/Rings Rotation/conditions/charging"] = false
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
+
+	else:											#Nothing
+		masterTree["parameters/Coils/conditions/coil"] = false
+		masterTree["parameters/Coils/conditions/uncoil"] = true
+		
+		masterTree["parameters/Rings Rotation/conditions/charging"] = false
+		masterTree["parameters/Rings Rotation/conditions/rotateFinish"] = true
+	
+	#Slow/speed up rotation speed
+	if(masterTree["parameters/Rings Rotation/conditions/charging"]):
+		if(rotationSpeed < 2.5):
+			rotationSpeed += 0.01
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
+	else:
+		if(rotationSpeed > 1):
+			rotationSpeed -= 0.05
+			masterTree["parameters/RingsSpeed/scale"] = rotationSpeed
