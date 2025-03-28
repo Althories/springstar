3750fa86d4edf32afab64e29ff92ed3ded7d0a32
Soampbar <Althories.bar@gmail.com>
2025-03-28 11:57:31 -0500

    LFG we fixed it
    I'm so happy chat.
    
    Comments updated for checkpoint script

--- Scripts/Checkpoints/checkpoint.gd
+++ Scripts/Checkpoints/checkpoint.gd
@@ -1,11 +1,12 @@
 extends Area3D
 #USAGE
-#--This general use area script can be edited for any area3D node.
-#Upon adding a checkpoint instance to a scene, connect the 
+#--This general use area script can be edited for any Checkpoint node
+#1. Upon adding a checkpoint instance to a scene, connect the 
 #'cp_pos' signal to the _on_cp_pos method of the spring CharacterBody3D.
 #--To edit the checkpoint collision, right click on the Checkpoint Area3D and select
-#'Make Local'
-#--To edit spring respawn location, change the position of the Checkpoint mesh.
+#'Make Local', then edit bounds
+#--To edit spring respawn location, change the position of the Checkpoint Area3D node.
+#The mesh is meant to be a visual aid and nothing more.
 
 #I don't think that's a thing (tragic) so maybe I can use an export var instead??
 #Like, fetch position of something in the checkpoint node and make the player respawn there
@@ -22,11 +23,9 @@ func _ready() -> void:
 	#warning: Messing with the tree hierarchy above SpringStuff may break this :[
 	#to fix, recopy spring path by dragging the Spring node into the script path area (remove $)
 	
-	#issue: The script doesn't wanna play ball
-	#it will only get one instance
 	spring = get_node("../../../SpringStuff/TestSpring")
-	respawn_mesh = get_node("Checkpoint_Respawn_Pos")
-	respawn_pos = respawn_mesh.global_position	#fetches objective mesh position in world
+	#respawn_mesh = get_node("Checkpoint_Respawn_Pos")
+	respawn_pos = global_position	#fetches objective Area3D position in world
 	
 func _process(_delta: float) -> void:
 	if overlaps_body(spring):
