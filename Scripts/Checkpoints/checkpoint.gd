732f218ab625a9303ab5441105a837d96c32c2a0
Soampbar <Althories.bar@gmail.com>
2025-03-28 11:05:47 -0500

    working checkpoint behavior
    finally.
    
    set up a completely functional checkpoint in tutorial

--- Scripts/Checkpoints/checkpoint.gd
+++ Scripts/Checkpoints/checkpoint.gd
@@ -1,25 +1,29 @@
 extends Area3D
 #USAGE
-#This general use area script can be edited for any area3D node.
+#--This general use area script can be edited for any area3D node.
 #Upon adding a checkpoint instance to a scene, connect the 
 #'cp_pos' signal to the _on_cp_pos method of the spring CharacterBody3D.
-
-#To edit the checkpoint collision, right click on the Checkpoint Area3D and select
+#--To edit the checkpoint collision, right click on the Checkpoint Area3D and select
 #'Make Local'
+#--To edit spring respawn location, change the position of the Checkpoint mesh.
 
 #I don't think that's a thing (tragic) so maybe I can use an export var instead??
 #Like, fetch position of something in the checkpoint node and make the player respawn there
-var respawn_pos = Vector3(0, 19, 0)		#This controls where the spring will respawn
+var respawn_pos = Vector3(0, 5, 0)		#This controls where the spring will respawn
 										#if the spring enters this area
 var spring	#This stores the path of the spring node 
 			#so that the area can check whether the spring overlaps it
 			#my only worry is whether this constant check impacts performance
+var respawn_mesh	#Initialized to fetch position of spring mesh 
+					#sends respawn position to spring script
 signal cp_pos(cp_position)
 
 func _ready() -> void:
 	#warning: Messing with the tree hierarchy above SpringStuff may break this :[
 	#to fix, recopy spring path by dragging the Spring node into the script path area (remove $)
 	spring = get_node("../../../SpringStuff/TestSpring")
+	respawn_mesh = get_node("Checkpoint_Respawn_Pos")
+	respawn_pos = respawn_mesh.global_position	#fetches objective mesh position in world
 	
 func _process(_delta: float) -> void:
 	if overlaps_body(spring):
