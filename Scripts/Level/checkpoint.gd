360c23bd94c5583dacd295e677de6f69a2879c3d
Soampbar <Althories.bar@gmail.com>
2025-03-28 14:03:28 -0500

    code cleanup
    created deathzone instance + script,
    
    cleaned up checkpoint script a bit for fun

--- Scripts/Level/checkpoint.gd
+++ Scripts/Level/checkpoint.gd
@@ -8,13 +8,10 @@ extends Area3D
 #--To edit spring respawn location, change the position of the Checkpoint Area3D node.
 #The mesh is meant to be a visual aid and nothing more.
 
-#I don't think that's a thing (tragic) so maybe I can use an export var instead??
-#Like, fetch position of something in the checkpoint node and make the player respawn there
 var respawn_pos = Vector3(0, 0, 0)		#This controls where the spring will respawn
 										#if the spring enters this area
 var spring	#This stores the path of the spring node 
 			#so that the area can check whether the spring overlaps it
-			#my only worry is whether this constant check impacts performance
 var respawn_mesh	#Initialized to fetch position of spring mesh 
 					#sends respawn position to spring script
 signal cp_pos(cp_position)
@@ -22,9 +19,7 @@ signal cp_pos(cp_position)
 func _ready() -> void:
 	#warning: Messing with the tree hierarchy above SpringStuff may break this :[
 	#to fix, recopy spring path by dragging the Spring node into the script path area (remove $)
-	
 	spring = get_node("../../../SpringStuff/TestSpring")
-	#respawn_mesh = get_node("Checkpoint_Respawn_Pos")
 	respawn_pos = global_position	#fetches objective Area3D position in world
 	
 func _process(_delta: float) -> void:
---
+++ Scripts/Level/death_zone.gd
@@ -0,0 +1,18 @@
+extends Area3D
+#USAGE
+#--This general use area script can be edited for any DeathZone node
+#1. Upon adding a checkpoint instance to a scene, connect the 
+#'destroy_spring' signal to the _on_cp_pos method of the spring CharacterBody3D.
+
+var spring	#This stores the path of the spring node 
+			#so that the area can check whether the spring overlaps it
+signal destroy_spring
+
+func _ready() -> void:
+	#warning: Messing with the tree hierarchy above SpringStuff may break this :[
+	#to fix, recopy spring path by dragging the Spring node into the script path area (remove $)
+	spring = get_node("../../../SpringStuff/TestSpring")
+	
+func _process(_delta: float) -> void:
+	if overlaps_body(spring):
+		pass
