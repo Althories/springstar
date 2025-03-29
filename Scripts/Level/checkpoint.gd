bf88252d4da42df66b2bd9298e2abd597c8673db
Soampbar <Althories.bar@gmail.com>
2025-03-28 22:30:31 -0500

    cleaned up checkpoint script
    - removed unused variabel
    - cleaned up comments a bit

--- Scripts/Level/checkpoint.gd
+++ Scripts/Level/checkpoint.gd
@@ -1,26 +1,24 @@
 extends Area3D
 #USAGE
-#--This general use area script can be edited for any Checkpoint node
+#--This general use script can be applied to any Checkpoint node
 #1. Upon adding a checkpoint instance to a scene, connect the 
 #'cp_pos' signal to the _on_cp_pos method of the spring CharacterBody3D.
-#--To edit the checkpoint collision, right click on the Checkpoint Area3D and select
-#'Make Local', then edit bounds
 #--To edit spring respawn location, change the position of the Checkpoint Area3D node.
 #The mesh is meant to be a visual aid and nothing more.
+#--To edit the checkpoint collision, right click on the Area3D and select
+#'Make Local', then edit collision bounds
 
-var respawn_pos = Vector3(0, 0, 0)		#This controls where the spring will respawn
-										#if the spring enters this area
-var spring	#This stores the path of the spring node 
-			#so that the area can check whether the spring overlaps it
-var respawn_mesh	#Initialized to fetch position of spring mesh 
-					#sends respawn position to spring script
+var respawn_pos = Vector3()		#Determines where the spring will respawn
+var spring						#Stores the path of the spring node 
+								#so that overlaps_body() can check for the spring
+
 signal cp_pos(cp_position)
 
 func _ready() -> void:
-	#warning: Messing with the tree hierarchy above SpringStuff may break this :[
+	#warning: Messing with the tree hierarchy above SpringStuff may break the path
 	#to fix, recopy spring path by dragging the Spring node into the script path area (remove $)
 	spring = get_node("../../../SpringStuff/TestSpring")
-	respawn_pos = global_position	#fetches objective Area3D position in world
+	respawn_pos = global_position	#fetches objective position of checkpoint in world
 	
 func _process(_delta: float) -> void:
 	if overlaps_body(spring):
