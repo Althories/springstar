6523597e3440f9d0de27189b145f57bde1d9f3b8
Soampbar <Althories.bar@gmail.com>
2025-03-28 22:54:52 -0500

    updated target.gd
    - added a bit of helper comments
    - removed unnecessary var

--- Scripts/SpringCharacter/target.gd
+++ Scripts/SpringCharacter/target.gd
@@ -1,7 +1,8 @@
 extends MeshInstance3D
+#Used in compass functionality. From compass.gd:
+#2. Connect target_pos signal from target.gd to _on_ship_part_target_pos in compass.gd
 
-signal target_pos(target_position)
+signal target_pos(target_position)		#sends target position for compass to point to
 
 func _process(_delta: float) -> void:
-	var x = position
-	target_pos.emit(x)
+	target_pos.emit(position)
