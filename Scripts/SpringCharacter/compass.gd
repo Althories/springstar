b988583ff8df519d08bb82fa4a29388eee0d24de
Soampbar <Althories.bar@gmail.com>
2025-03-28 22:51:48 -0500

    changed compass.gd
    really didn't have to change much here

--- Scripts/SpringCharacter/compass.gd
+++ Scripts/SpringCharacter/compass.gd
@@ -1,6 +1,5 @@
 extends Node3D
-
-#WARNING - for importing compass into new scene
+#USAGE - for importing compass into new scene
 #If you are importing the compass and a ship part into a new scene, 
 #you must hook up 2 signals before the compass will point to a ship part.
 #1. Connect spring_pos signal from spring_script.gd to _on_test_spring_spring_pos in this script
@@ -10,7 +9,7 @@ extends Node3D
 var target = Vector3()
 
 func _ready():
-	visible = false
+	visible = false						#hides compass on startup
 
 func _process(_delta: float) -> void:
 	#Determine whether compass is shown to the player from input
@@ -22,7 +21,7 @@ func _process(_delta: float) -> void:
 	#Calculate the direction from the pointer to the target
 	var direction_to_target = (target - compass.global_transform.origin).normalized()
 	var angle_y = atan2(direction_to_target.x, direction_to_target.z)
-	compass.rotation_degrees.y = rad_to_deg(angle_y) - 90
+	compass.rotation_degrees.y = rad_to_deg(angle_y) - 90		#rotate compass mesh accordingly
 	
 func _on_ship_part_target_pos(target_position: Variant) -> void:
 	target = target_position
