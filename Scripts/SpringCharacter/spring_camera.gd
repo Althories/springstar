155fc4f6cdca7a3ce984025afe77ded991d5c509
Soampbar <Althories.bar@gmail.com>
2025-04-16 17:25:17 -0500

    it's as shrimple as that
    - adjustable fov implemented
    - camera script for it
    - bound to 1 and 2 by default

--- Scripts/SpringCharacter/spring_camera.gd
+++ Scripts/SpringCharacter/spring_camera.gd
@@ -1,6 +1,13 @@
 extends Camera3D
 
-var spring_fov = fov
+var target_fov = 75.0		#default set in scene
 
-func _process(float) -> void:
-	pass
+func _input(event):
+	if event.is_action_pressed("fov_up"):
+		target_fov += 5
+	if event.is_action_pressed("fov_down"):
+		target_fov -= 5
+	
+func _process(_float) -> void:
+	fov = lerp(fov, target_fov, .1)		#im lerping it ough
+										#smooths fov from current to desired value by a certain rate from 0 to 1
