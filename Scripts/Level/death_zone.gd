d9696d8764b3fade1c7519bce593429d04dd3f5e
Soampbar <Althories.bar@gmail.com>
2025-04-17 10:13:36 -0500

    - hazard added
    - it works.
    - added some comments to explain the blender to godot pipeline for this better

--- Scripts/Level/death_zone.gd
+++ Scripts/Level/death_zone.gd
@@ -5,13 +5,13 @@ extends Area3D
 #'destroy_spring' signal to the _on_destroy_spring method of the spring CharacterBody3D.
 
 #-- To use this DeathZone for a visible "hazard" asset:
-#1. Import the mesh you want to use for the deathzone
-#2. Generate a collisionshape for the imported mesh
-#3. Delete the staticBody3D node
-#4. Make the generated mesh collision a direct child of the DeathZone node
+#1. Make new scene w/3D node, import asset with root type Area3D
+#2. Make asset local and delete 3D node 
+#3. Generate a collisionshape for the imported mesh
+#4. Delete the staticBody3D node
+#5. Make the generated mesh collision a direct child of the Area3D
+#6. Attach this script to the Area3D
 
-#warning: Messing with the tree hierarchy above SpringStuff may break this path
-#to fix, recopy spring path by dragging the Spring node into the script path area (remove $)
 #Stores the path of the spring node so that the area can check whether the spring overlaps it
 @onready var spring = get_node("%TestSpring")
 
