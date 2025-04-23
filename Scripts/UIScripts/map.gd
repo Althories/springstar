813e17935d3d0cbefd75deff8b68fa91250c247b
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-04-23 11:03:05 -0500

    Tutorial map functioning

---
+++ Scripts/UIScripts/map.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var mapBG: ColorRect = $background
+@onready var mapImage: Sprite2D = $background/map
+@onready var charCursor: Sprite2D = $background/map/character
+
+@export var character: CharacterBody3D
+
+const POS_SCALE = 3.2
+
+func _ready() -> void:
+	mapBG.visible = false
+
+func _physics_process(delta: float) -> void:
+	charCursor.position = Vector2(character.position.x*POS_SCALE, character.position.z*POS_SCALE)
+	charCursor.rotation = -character.rotation.y
+	if Input.is_action_pressed("map"):
+		mapBG.visible = true
+	else:
+		mapBG.visible = false
