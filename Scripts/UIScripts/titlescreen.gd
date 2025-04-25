e0647099e8b4d9205c8f8e3cfda95f3ec2b42752
Soampbar <Althories.bar@gmail.com>
2025-04-25 13:14:29 -0500

    script written
    i gotta get back to my pc before i can do more lol

--- Scripts/UIScripts/titlescreen.gd
+++ Scripts/UIScripts/titlescreen.gd
@@ -1,6 +1,10 @@
 extends Control
 
+@onready var intro_video = get_node("$Intro_Video_Player")
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/tutorial.tscn")
-	pass # Replace with function body.
+	intro_video.play()
+	
+func _process(_float) -> void:
+	if Input.is_action_pressed("skip"):
+		get_tree().change_scene_to_file("res://Scenes/Levels/tutorial.tscn")
