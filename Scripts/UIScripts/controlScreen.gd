f7c509bfe6c40a119f32855b1cdd26e43c07b191
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-04-28 11:06:27 -0500

    control screen made

--- Scripts/UIScripts/controlScreen.gd
+++ Scripts/UIScripts/controlScreen.gd
@@ -6,3 +6,4 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("controls"):
 		self.visible = not self.visible
+		get_tree().paused = not get_tree().paused
